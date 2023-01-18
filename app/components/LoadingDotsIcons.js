599e5a9362499838ead0948982487db843fc5616
Indira Pandey <indubhusal@gmail.com>
2023-01-17 16:22:38 -0800

    Working on Edit and Post Action

---
+++ app/components/LoadingDotsIcons.js
@@ -0,0 +1,11 @@
+import React, { useEffect } from "react";
+
+function LoadingDotsIcons() {
+  return (
+    <div className="dots-loading">
+      <div></div>
+    </div>
+  );
+}
+
+export default LoadingDotsIcons;
---
+++ app/components/ViewSinglePost.js
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from "react";
+import Page from "./Page";
+import { useParams, Link } from "react-router-dom";
+import Axios from "axios";
+import LoadingDotsIcons from "./LoadingDotsIcons";
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+import ReactTooltip from "react-tooltip";
+
+function ViewSinglePost() {
+  const { id } = useParams();
+  const [isLoading, setIsLoading] = useState(true);
+  const [post, setPost] = useState([]);
+
+  useEffect(() => {
+    const ourRequest = Axios.CancelToken.source();
+    async function fetchPost() {
+      try {
+        const response = await Axios.get(`/post/${id}`, { cancelToken: ourRequest.token });
+        setPost(response.data);
+        setIsLoading(false);
+      } catch (e) {
+        console.log("There was a problem or the request was cancelled.");
+      }
+    }
+    fetchPost();
+    return () => {
+      ourRequest.cancel();
+    };
+  }, []);
+
+  if (isLoading)
+    return (
+      <Page title="...">
+        <LoadingDotsIcons />
+      </Page>
+    );
+  const date = new Date(post.createdDate);
+  const dateFormatted = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+  return (
+    <Page title={post.title}>
+      <div className="d-flex justify-content-between">
+        <h2>{post.title}</h2>
+        <span className="pt-2">
+          <Link to={`/post/${post._id}/edit`} data-tip="Edit" data-for="edit" className="text-primary mr-2" title="Edit">
+            <i className="fas fa-edit"></i>
+          </Link>
+          {"   "}
+          <a className="delete-post-button text-danger" title="Delete">
+            <i className="fas fa-trash"></i>
+          </a>
+        </span>
+      </div>
+
+      <p className="text-muted small mb-4">
+        <Link to={`/profile/${post.author.username}`}>
+          <img className="avatar-tiny" src={post.author.avatar} />
+        </Link>
+        Posted by <Link to={`/profile/${post.author.username}`}>{post.author.username}</Link> on {dateFormatted}
+      </p>
+
+      <div className="body-content">
+        <ReactMarkdown children={post.body} allowedElements={["p", "br", "strong", "em", "h1", "h2", "h3", "h4", "h5", "h6", "ul", "ol", "li"]} />
+      </div>
+    </Page>
+  );
+}
+
+export default ViewSinglePost;
